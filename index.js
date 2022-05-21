8e4ed674f31309457f38a24167494734f36bb14e
William Roton <wroton@structdevelopment.com>
2022-05-21 16:41:37 -0500

    Remove email for now.

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
 let toast = undefined;
 let toastCleared = false;
 let toastTimeout = undefined;
+
 // Gets rid of the toast.
 function clearToast() {
-  // No toast or already cleared, then don't clear.
-  if (toastCleared || !toast) {
-    return;
-  }
-
-  // Set flags.
-  toastCleared = true;
-
-  // Remove the visible.
-  toast.className = toast.className.replace(" visible", " hidden");
-
-  // Clear toast.
-  toast = undefined;
 }
 
 // Pauses toast dismissal.
 function pauseToast() {
-  clearTimeout(toastTimeout);
 }
 
 // Resumes toast dismissal.
 function resumeToast(timeout = 2750) {
-  toastTimeout = setTimeout(clearToast, timeout);
 }
 
 // Checks for the success value in the URL.
 function successCheck() {
-  // Load the URL parameters.
-  var urlParameters = new URLSearchParams(location.search);
-
-  // Check for the success value.
-  if (urlParameters.has("success")) {
-    const success = urlParameters.get("success");
-    if (success === "1") {
-      toast = document.getElementById("good-toast");
-      toast.className = "toast visible";
-    } else if (success === "0") {
-      toast = document.getElementById("bad-toast");
-      toast.className = "toast bad visible";
-    }
-  }
-
-  // Start timeout.
-  resumeToast();
 }
 
 let collapsed = true;
