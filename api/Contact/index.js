021c610af767ae4c1e8f827afe7a91719bf23ee0
William Roton <wroton@structdevelopment.com>
2022-05-29 16:22:35 -0500

    Bad request handling.

--- api/Contact/index.js
+++ api/Contact/index.js
@@ -1,5 +1,14 @@
 module.exports = async function (context, req) {
   if (req && req.rawBody && req.rawBody.length < 3000) {
     console.log(req.body);
+    context.res = {
+      status: 201
+    };
+    
+    return;
   }
+
+  context.res = {
+    status: 413
+  };
 }
