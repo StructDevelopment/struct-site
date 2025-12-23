72a0e5489aa1065bc97f8cae6768bee0f02de506
William Roton <wroton@structdevelopment.com>
2025-12-23 16:17:09 -0600

    Trace messaging replaced with emailing.

--- api/Contact/index.js
+++ api/Contact/index.js
@@ -1,6 +1,31 @@
+const { EmailClient } = require("@azure/communication-email");
+
+const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
+const emailClient = new EmailClient(connectionString);
+
 module.exports = async function (context, req) {
   if (req && req.rawBody && req.rawBody.length < 3000) {
-    console.log(req.rawBody);
+    const { email, first, last, message, phone } = req.body;
+    const emailMessage = {
+      senderAddress: "donotreply@structdevelopment.com",
+      content: {
+        subject: "Contact Request Received",
+        plainText: `Contact request received. '${first} ${last}' sent the following message: '${message}'. They may be contacted at email address '${email}' or '${phone}'.`
+      },
+      recipients: {
+        to: [{ address: "struct@structdevelopment.com" }],
+      }
+    };
+
+    try {
+      const poller = await emailClient.beginSend(emailMessage);
+      await poller.pollUntilDone();
+      
+      console.log(`Contact email sent. JSON body sent: '${req.rawBody}'.`);
+    } catch (err) {
+      console.error(`Contact email failed. ${err}. JSON body dropped: '${req.rawBody}'.`);
+    }
+
     context.res = {
       status: 201
     };
