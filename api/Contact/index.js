aac6e472cf3509eceb8d688f5e14ea5823842589
William Roton <wroton@structdevelopment.com>
2025-12-23 16:58:48 -0600

    Reply to address fix.

--- api/Contact/index.js
+++ api/Contact/index.js
@@ -7,7 +7,7 @@ module.exports = async function (context, req) {
   if (req && req.rawBody && req.rawBody.length < 3000) {
     const { email, first, last, message, phone } = req.body;
     const emailMessage = {
-      senderAddress: "donotreply@structdevelopment.com",
+      senderAddress: "DoNotReply@5f466b15-e008-4b0d-987d-525e9f6d4746.azurecomm.net",
       content: {
         subject: "Contact Request Received",
         plainText: `Contact request received. '${first} ${last}' sent the following message: '${message}'. They may be contacted at email address '${email}' or '${phone}'.`
